dd902598058855e415d0d6f094bcc7585b1f2444
tie209192 <tie209192@S1706C10854-T1.tisnt.tis.co.jp>
2018-06-14 18:25:20 +0900

    aaa

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -2,9 +2,22 @@ package codecheck;
 
 public class App {
 	public static void main(String[] args) {
-		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
-			System.out.println(output);
+//		for (int i = 0, l = args.length; i < l; i++) {
+//			String output = String.format("argv[%s]: %s", i, args[i]);
+//			System.out.println(output);
+
+
+
+		String apiUrl = "http://challenge-server.code-check.io/";
+
+			URL connectUrl = new URL(apiUrl);
+				HttpURLConnection con = (HttpURLConnection)connectUrl.openConnection();
+
+				con.setRequestMethod("GET");
+				con.setDoOutput(true);
+				con.setInstanceFollowRedirects(true);
+
+
 		}
 	}
 }
