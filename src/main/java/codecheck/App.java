f34da8d4dd5c3b048b8723799a59083ac1bf1273
tie209192 <tie209192@S1706C10854-T1.tisnt.tis.co.jp>
2018-06-22 21:00:50 +0900

    aaaaaa

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -1,23 +1,21 @@
 package codecheck;
 
+import javax.ws.rs.client.WebTarget;
+
 public class App {
 	public static void main(String[] args) {
 //		for (int i = 0, l = args.length; i < l; i++) {
 //			String output = String.format("argv[%s]: %s", i, args[i]);
 //			System.out.println(output);
 
+		WebTarget target = client.target("http://challenge-server.code-check.io/")
+		 .queryParam("q", "hash");
 
-
-		String apiUrl = "http://challenge-server.code-check.io/";
-
-			URL connectUrl = new URL(apiUrl);
-				HttpURLConnection con = (HttpURLConnection)connectUrl.openConnection();
-
-				con.setRequestMethod("GET");
-				con.setDoOutput(true);
-				con.setInstanceFollowRedirects(true);
-
-
-		}
+			try {
+			    String result = target.request().get(String.class);
+			} catch (BadRequestException e) {
+			    logger.error("response=" + e.getResponse().readEntity(String.class), e);
+			    throw e;
+			}
 	}
 }
